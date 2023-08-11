106ff6a458818f53a50b1846f5ea107da5ae3ef4
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-11 17:23:18 +0300

    add catch all routes

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function BlogPostsPage() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>The Blog Posts</h1>
+    </div>
+  );
+}
